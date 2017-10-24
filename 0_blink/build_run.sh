6e50625673643036a20cf2484637c1f410c31072
Carlos Galvez <carlosgalvezp@gmail.com>
2017-10-24 21:16:56 +0200

    Use the stm32-dev docker image in the build script

--- 0_blink/build_run.sh
+++ 0_blink/build_run.sh
@@ -1,15 +1,31 @@
 #!/bin/bash
+set -e
+
+# Define docker run call
+THIS_DIR=$(dirname $(readlink -f "$0"))
+DOCKER_RUN="docker run --rm=true                        \
+                       --tty=true                       \
+                       --interactive=true               \
+                       --volume=${THIS_DIR}:${THIS_DIR} \
+                       --workdir=${THIS_DIR}            \
+                       --device=/dev/ttyUSB0            \
+                       carlosgalvezp/stm32-dev"
+
 # Compile
-arm-none-eabi-g++ -c -mcpu=cortex-m3 -mthumb --std=c++14 -O2 -fno-rtti -fno-exceptions main.cpp -o main.o
+echo "Compiling..."
+$DOCKER_RUN arm-none-eabi-g++ -c -mcpu=cortex-m3 -mthumb --std=c++14 -O2 -fno-rtti -fno-exceptions main.cpp -o main.o
 
 # Link
-arm-none-eabi-g++ -mcpu=cortex-m3 -mthumb -Tstm32f103c8t6.ld -nostartfiles main.o -o main.elf
+echo "Linking..."
+$DOCKER_RUN arm-none-eabi-g++ -mcpu=cortex-m3 -mthumb -Tstm32f103c8t6.ld -nostartfiles main.o -o main.elf
 
 # Create bin
-arm-none-eabi-objcopy -O binary main.elf main.bin
+echo "Creating bin..."
+$DOCKER_RUN arm-none-eabi-objcopy -O binary main.elf main.bin
 
 # Write to flash
-stm32flash -w main.bin /dev/ttyUSB0
+echo "Deploying..."
+$DOCKER_RUN stm32flash -w main.bin /dev/ttyUSB0
 
 # Run
-stm32flash -g0 /dev/ttyUSB0
+$DOCKER_RUN stm32flash -g0 /dev/ttyUSB0
