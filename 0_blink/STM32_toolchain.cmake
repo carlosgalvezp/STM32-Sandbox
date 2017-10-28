ff5ef8d7686405e3392581fe3fa17188b75a9dd2
Carlos Galvez <carlosgalvezp@gmail.com>
2017-10-28 10:11:56 +0200

    Remove deprecated ForceCompiler CMake option

--- 0_blink/STM32_toolchain.cmake
+++ 0_blink/STM32_toolchain.cmake
@@ -1,10 +1,11 @@
-INCLUDE(CMakeForceCompiler)
-
 SET(CMAKE_SYSTEM_NAME Linux)
 
+# Avoid linking test program, since it requires linker script
+SET(CMAKE_TRY_COMPILE_TARGET_TYPE STATIC_LIBRARY)
+
 # Specify the cross compiler
-CMAKE_FORCE_C_COMPILER(arm-none-eabi-gcc GNU)
-CMAKE_FORCE_CXX_COMPILER(arm-none-eabi-g++ GNU)
+SET(CMAKE_C_COMPILER arm-none-eabi-gcc)
+SET(CMAKE_CXX_COMPILER arm-none-eabi-g++)
 
 # Specify the objcopy command
 SET(CMAKE_OBJCOPY_CMD arm-none-eabi-objcopy)
